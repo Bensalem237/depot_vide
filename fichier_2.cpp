13beb4f7387d3ddf7eb4f6a6a4092c781f0d96b2
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-19 20:42:44 +0100

    Prolonged the array

--- fichier_2.cpp
+++ fichier_2.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 int main() {
-    std::vector<int> numbers = {1, 2, 3, 4, 5};
+    std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8. 9. 10};
     
     std::cout << "Numbers in the vector: ";
     for (const auto& num : numbers) {
