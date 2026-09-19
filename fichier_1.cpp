9c6e362b92272754c150b76e26c141164ca08118
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-19 04:14:06 +0100

    Ajout de deux lignes au code

--- fichier_1.cpp
+++ fichier_1.cpp
@@ -2,5 +2,7 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Le Monde est tèllement beau!!!" << std::endl;
+    std::cout << "Mais je dis la vérité hein, le monde est beau gars" << std:endl;
     return 0;
 }
