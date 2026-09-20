b52b0eee834fec717e43647028bade7b19896fc7
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-20 17:12:46 +0100

    :)

--- fichier_1.cpp
+++ fichier_1.cpp
@@ -4,6 +4,6 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Le Monde est tèllement beau!!!" << std::endl;
     std::cout << "Mais je dis la vérité hein, le monde est beau gars" << std:endl;
-    std::cout << "C'est quand meme vrai qu'il y a des endroits qui sont laids... mais dans l'ensemble le monde est quand meme beau :)" << std::endl;
+    std::cout << "C'est quand meme vrai qu'il y a des endroits qui sont laids... mais dans l'ensemble le monde est quand meme beau *h" << std::endl;
     return 0;
 }
