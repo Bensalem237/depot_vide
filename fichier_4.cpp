5de390fdd80f4ed98971e2119ce93fd5e3d2c758
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-20 03:21:49 +0000

    Deuxième commit

--- fichier_4.cpp
+++ fichier_4.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-    std::cout << "Hello World"  << std::endl;
+    int x
+    std::cout << "Hello World !"  << std::endl;
+    std::cout << "Entrez un nombre : ";
+    std::cin >> x;
+    std::cout << "Ça racine carrée est " << std::sqrt(x) << std::endl;
+
     return 0;
 }
 
