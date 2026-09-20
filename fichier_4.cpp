25361523f5e4d82b49505ad70a2a6bbe95509df9
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-20 18:57:44 +0100

    exo8

--- fichier_4.cpp
+++ fichier_4.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 int main() {
-    int x, n1, n2;
+    int x, n1, n2, entier;
     std::cout << "Hello World !"  << std::endl;
     std::cout << "Entrez un nombre : ";
     std::cin >> x;
