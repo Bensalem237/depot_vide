3e83dc66dee6ed8047b1b73425b3c95a16401934
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-20 03:25:54 +0000

    Troisième commit

--- fichier_4.cpp
+++ fichier_4.cpp
@@ -2,11 +2,15 @@
 #include <cmath>
 
 int main() {
-    int x
+    int x, n1, n2;
     std::cout << "Hello World !"  << std::endl;
     std::cout << "Entrez un nombre : ";
     std::cin >> x;
-    std::cout << "Ça racine carrée est " << std::sqrt(x) << std::endl;
+    std::cout << "Sa racine carrée est " << std::sqrt(x) << std::endl;
+    std::cout << "C'est bien ça !" << std::endl;
+    std::cout << "Maintenant, entrez deux entiers : ";
+    std::cin >> n1 >> n2;
+    std::cout << "Leur somme est " << n1 + n2 << std::endl;
 
     return 0;
 }
