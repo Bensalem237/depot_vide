91d75dfb640511691f7ae16e0ef9a743e0b1f308
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-21 03:58:46 +0100

    Encore un commit sur la branche principale

--- fichier_1.cpp
+++ fichier_1.cpp
@@ -4,5 +4,6 @@ int main() {
     int a = 99;
     std::cout << "Hello, World!" << std::endl;
     std::cout << "La valeur de a est :" << a << std::endl;
+    std::cout << "C++17 is cool" << std:endl;
     return 0;
 }
