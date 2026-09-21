937e97c62be5a1c6a376323ca3c8e5195808a481
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-21 03:54:46 +0100

    Premier commit sur fonctionnalite_1 (Rejouer)

--- fichier_4.cpp
+++ fichier_4.cpp
@@ -11,6 +11,7 @@ int main() {
     std::cout << "Maintenant, entrez deux entiers : ";
     std::cin >> n1 >> n2;
     std::cout << "Leur somme est " << n1 + n2 << std::endl;
+    std::cout << "C++17" << std::endl;
 
     return 0;
 }
