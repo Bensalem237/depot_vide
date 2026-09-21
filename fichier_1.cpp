3dfee30ada41b723a4fb85f95b87090368bec543
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-21 03:39:02 +0100

    Deuxieme commit fonctionnalite_1

--- fichier_1.cpp
+++ fichier_1.cpp
@@ -3,6 +3,6 @@
 int main() {
     int a = 99;
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "La valeur de a est : << a << std::endl;
+    std::cout << "La valeur de a est :" << a << std::endl;
     return 0;
 }
