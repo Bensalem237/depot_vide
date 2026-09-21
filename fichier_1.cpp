6275f67ce1f948a113ea6c926e8d6804cae31eff
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-21 03:37:41 +0100

    Premier commit fonctionnalite_1

--- fichier_1.cpp
+++ fichier_1.cpp
@@ -3,8 +3,6 @@
 int main() {
     int a = 99;
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Le Monde est tèllement beau!!!" << std::endl;
-    std::cout << "Mais je dis la vérité hein, le monde est beau gars" << std:endl;
-    std::cout << "C'est quand meme vrai qu'il y a des endroits qui sont laids... mais dans l'ensemble le monde est quand meme beau *h" << std::endl;
+    std::cout << "La valeur de a est : << a << std::endl;
     return 0;
 }
